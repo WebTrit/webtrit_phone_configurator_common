f90557298b20da5ce02770108a130e2d50620d18
Yurii Cherniavskyi <ych@webtrit.com>
2023-12-06 16:46:07 +0200

    feat: initialize WebTrit Phone Configurator Common package
    To create the package following command was executed:
    $ dart create --template=package webtrit_phone_configurator_common

---
+++ example/webtrit_phone_configurator_common_example.dart
@@ -0,0 +1,6 @@
+import 'package:webtrit_phone_configurator_common/webtrit_phone_configurator_common.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/webtrit_phone_configurator_common.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/webtrit_phone_configurator_common_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
